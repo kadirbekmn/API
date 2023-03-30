0aaa1f1b25cf778a91b5a200bbce322637362e49
Sulayman Matkuliyev <suleymantm11@gmail.com>
2023-03-30 18:18:03 +0500

    infrastructure

--- src/main/java/com/example/getData/controller/GetController.java
+++ src/main/java/com/example/getData/controller/GetController.java
@@ -1,6 +1,20 @@
 package com.example.getData.controller;
 
+import com.example.getData.service.GetService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@RestController
+@AllArgsConstructor
+@Produces(MediaType.APPLICATION_JSON)
+@RequestMapping()
 public class GetController {
+
+    private GetService service;
 
 
 
--- src/main/java/com/example/getData/service/GetService.java
+++ src/main/java/com/example/getData/service/GetService.java
@@ -1,4 +1,11 @@
 package com.example.getData.service;
 
+import com.example.getData.repository.GetRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
 public class GetService {
+    private GetRepository repository;
 }
